d0529929d8a12c7c3a735376da73f6223628b98a
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-08-09 21:59:40 +0100

    wait group implementation

---
+++ wait-groups/wait-group-implementation/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"sync/atomic"
+	"time"
+)
+
+type waitGroup struct {
+	counter int64
+}
+
+func (wg *waitGroup) Add(n int64) {
+	atomic.AddInt64(&wg.counter, n)
+}
+
+func (wg *waitGroup) Done() {
+	wg.Add(-1)
+	if atomic.LoadInt64(&wg.counter) < 0 {
+		panic("negative wait group counter")
+	}
+}
+
+func (wg *waitGroup) Wait() {
+	for {
+		if atomic.LoadInt64(&wg.counter) == 0 {
+			return
+		}
+	}
+}
+
+func main() {
+	var wg waitGroup
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		time.Sleep(300 * time.Microsecond)
+		println("go routine 1")
+	}()
+	go func() {
+		defer wg.Done()
+		time.Sleep(500 * time.Microsecond)
+		println("go routine 2")
+	}()
+	wg.Wait()
+	println("all go routines are done")
+}
